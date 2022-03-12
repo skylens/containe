81b745aae212adadfe5c675e44c529095ad30d29
skylens <skylens116@outlook.com>
2022-03-13 00:20:06 +0800

    update

--- wireguard/installdeps.sh
+++ wireguard/installdeps.sh
@@ -2,5 +2,5 @@
 
 apt-get update -y
 apt-get install -y apt-utils
-apt-get install -y --no-install-recommends iptables
+apt-get install -y --no-install-recommends iptables ca-certificates net-tools
 update-ca-certificates --fresh
